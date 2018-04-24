c03c3a8e29d770929abc81c527da6e821231d060
HelloKittycoder <shucheng2015@outlook.com>
2018-04-24 23:03:43 +0800

    ExcelUtils使用demo

---
+++ ExcelLearning/excelUtilsDemo/src/com/kittycoder/easydemo/ExcelReportTest.java
@@ -0,0 +1,61 @@
+package com.kittycoder.easydemo;
+
+import com.kittycoder.po.Student;
+import net.sf.excelutils.ExcelUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shucheng on 2018/4/24.
+ * ExcelUtils模板导出示例
+ * 参考：http://excelutils.sourceforge.net/
+ */
+public class ExcelReportTest {
+
+    public static void main(String[] args) {
+        try {
+            // 准备数据
+            List<Student> studentList = new ArrayList<Student>();
+            Student student = new Student();
+            student.setSid("101");
+            student.setSname("张三");
+            student.setSphone("13101230123");
+            student.setSaddress("北京");
+            studentList.add(student);
+
+            student = new Student();
+            student.setSid("102");
+            student.setSname("李四");
+            student.setSphone("15101230123");
+            student.setSaddress("上海");
+            studentList.add(student);
+
+            student = new Student();
+            student.setSid("103");
+            student.setSname("王五");
+            student.setSphone("17101230123");
+            student.setSaddress("广州");
+            studentList.add(student);
+
+            student = new Student();
+            student.setSid("104");
+            student.setSname("赵六");
+            student.setSphone("18101230123");
+            student.setSaddress("深圳");
+            studentList.add(student);
+
+            ExcelUtils.addValue("studentList", studentList);
+
+            String projectPath = System.getProperty("user.dir");
+            String templatePath = projectPath + "\\template\\studentInfo.xls"; // 模版文件路径
+            File outputFile = new File("导出学生信息表.xls");
+            ExcelUtils.export(templatePath, new FileOutputStream(outputFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ ExcelLearning/excelUtilsDemo/src/com/kittycoder/po/Student.java
@@ -0,0 +1,44 @@
+package com.kittycoder.po;
+
+/**
+ * Created by shucheng on 2018/4/24.
+ */
+public class Student {
+
+    private String sid; // 学号
+    private String sname; // 姓名
+    private String sphone; // 手机
+    private String saddress; // 地址
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSphone() {
+        return sphone;
+    }
+
+    public void setSphone(String sphone) {
+        this.sphone = sphone;
+    }
+
+    public String getSaddress() {
+        return saddress;
+    }
+
+    public void setSaddress(String saddress) {
+        this.saddress = saddress;
+    }
+}
