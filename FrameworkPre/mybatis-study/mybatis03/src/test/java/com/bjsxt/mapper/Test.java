1121a475d270fdc6be9c716628eabdc7f645399c
HelloKittycoder <shucheng2015@outlook.com>
2019-03-18 21:49:12 +0800

    add mysql pagination

--- FrameworkPre/mybatis-study/mybatis03/src/test/java/com/bjsxt/mapper/Test.java
+++ FrameworkPre/mybatis-study/mybatis03/src/test/java/com/bjsxt/mapper/Test.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,11 +35,22 @@ public class Test {
         System.out.println(flower);*/
 
         // 传入一个Map参数
-        Map<String, Object> map = new HashMap<>();
+        /*Map<String, Object> map = new HashMap<>();
         map.put("id", 2);
         map.put("name", "百日草");
         Flower flower = session.selectOne("a.b.selectById2", map);
-        System.out.println(flower);
+        System.out.println(flower);*/
+
+        // 分页查询
+        Map<String, Object> map = new HashMap<>();
+        // 显示几个
+        int pageSize = 2;
+        // 第几页
+        int pageNumber = 2;
+        map.put("pageStart", (pageNumber-1)*pageSize);
+        map.put("pageSize", pageSize);
+        List<Flower> flowerList = session.selectList("a.b.page", map);
+        System.out.println(flowerList);
         session.close();
     }
 }
