6eb981de742f6505045bbe11f512608d6d5c5a2a
HelloKittycoder <shucheng2015@outlook.com>
2019-05-18 23:00:18 +0800

    Update DemoController.java

--- FrameworkPre/springmvc-study/springmvc08/src/main/java/com/bjsxt/controller/DemoController.java
+++ FrameworkPre/springmvc-study/springmvc08/src/main/java/com/bjsxt/controller/DemoController.java
@@ -68,43 +68,4 @@ public class DemoController {
         mav.addObject("mavPeo", peo);
         return mav;
     }
-
-    /*// 在servlet中的写法
-    @RequestMapping("demo")
-    public void demo(HttpServletResponse resp) throws Exception {
-        People p = new People();
-        p.setAge(11);
-        p.setName("张三");
-        ObjectMapper mapper = new ObjectMapper();
-        String result = mapper.writeValueAsString(p);
-
-        resp.setContentType("application/json;charset=utf-8"); // 设置响应头，解决中文乱码
-        PrintWriter out = resp.getWriter();
-        out.print(result);
-        out.flush();
-        out.close();
-
-        System.out.println("执行了demo");
-    }
-
-    @RequestMapping("demo2")
-    @ResponseBody // 1. 将People转换为json 2.将响应头设置为application/json
-    public People demo2() {
-        People p = new People();
-        p.setAge(11);
-        p.setName("张三");
-        System.out.println("执行了demo2");
-        return p;
-    }
-
-    // produces 设置响应头中Content-Type的取值
-    @RequestMapping(value = "demo3", produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String demo3() {
-        People p = new People();
-        p.setAge(11);
-        p.setName("张三");
-        System.out.println("执行了demo3");
-        return "中文";
-    }*/
 }
