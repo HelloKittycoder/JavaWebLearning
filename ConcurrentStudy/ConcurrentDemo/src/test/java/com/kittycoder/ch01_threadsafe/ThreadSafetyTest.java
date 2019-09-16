4fdb606b5e02aa57fad332b5b800a833f4f4390a
HelloKittycoder <shucheng2015@outlook.com>
2019-09-16 09:33:32 +0800

    并发编程-线程安全测试demo，弄明白可能的代码执行次序

--- ConcurrentStudy/ConcurrentDemo/src/test/java/com/kittycoder/ch01_threadsafe/ThreadSafetyTest.java
+++ ConcurrentStudy/ConcurrentDemo/src/test/java/com/kittycoder/ch01_threadsafe/ThreadSafetyTest.java
@@ -6,6 +6,18 @@ package com.kittycoder.ch01_threadsafe;
  *
  * 线程安全概念：当多个线程访问某一个类（对象或方法时），这个对象试中都能表现出正确的行为，那么这个类（对象或方法）就是线程安全的
  * synchronized：可以在任意对象及方法上加锁，而加锁的这段代码称为“互斥区”或“临界区”
+ *
+ * 比如这种打印结果：
+ * t3 count=2
+ * t2 count=2
+ * t5 count=0
+ * t1 count=1
+ * t4 count=1
+ *
+ * run方法里的代码执行顺序可能是这样的：
+ * ----------|--t1--|--t2--|--t3--|--t4--|--t5--|
+ * count--   |  1   |  6   |  9   |  2   |  3   |
+ * count     |  4   |  5   |  10  |  7   |  8   |
  */
 public class ThreadSafetyTest extends Thread {
 
