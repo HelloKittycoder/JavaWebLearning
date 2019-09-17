f77d56301bc3d68f0274032a674520154dd23ba0
HelloKittycoder <shucheng2015@outlook.com>
2019-09-17 23:38:49 +0800

    并发编程-volatile关键字不具备原子性

---
+++ ConcurrentStudy/ConcurrentDemo/src/test/java/com/kittycoder/ch07/VolatileNoAtomic.java
@@ -0,0 +1,38 @@
+package com.kittycoder.ch07;
+
+/**
+ * Created by shucheng on 2019-9-17 下午 22:12
+ * volatile关键字不具备synchronized关键字的原子性（同步）
+ * 如果使用int count，对于打印出来的结果不太理解（为什么最终结果达不到1000？）
+ *
+ * https://www.cnblogs.com/zhengbin/p/5653051.html
+ */
+public class VolatileNoAtomic extends Thread {
+
+    private static volatile int count;
+    // private static AtomicInteger count = new AtomicInteger(0);
+
+    private static void addCount() {
+        for (int i = 0; i < 1000; i++) {
+            count++;
+            // count.incrementAndGet();
+        }
+        System.out.println(count);
+    }
+
+    public void run() {
+        addCount();
+    }
+
+    public static void main(String[] args) {
+
+        VolatileNoAtomic[] arr = new VolatileNoAtomic[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = new VolatileNoAtomic();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            arr[i].start();
+        }
+    }
+}
