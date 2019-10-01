648972b21ab4923f1cc80317e825da7ef0a54e96
HelloKittycoder <shucheng2015@outlook.com>
2019-10-01 21:44:25 +0800

    netty模拟http请求与响应demo

--- NetworkProgramming/SocketIO_03/README.md
+++ NetworkProgramming/SocketIO_03/README.md
@@ -1,3 +1,4 @@
 ### Netty
 数据通信demo runtime包  
 心跳检测demo heartBeat包  
+模拟http请求与响应demo http包  
---
+++ NetworkProgramming/SocketIO_03/src/test/java/com/kittycoder/http/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,29 @@
+package com.kittycoder.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * Created by shucheng on 2019-10-1 下午 21:22
+ */
+public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpHelloWorldServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpHelloWorldServerHandler());
+    }
+}
